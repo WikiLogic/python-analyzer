b24b4230533715d6bc95826a0ac51c36c5ce16a0
ijmccallum <ijmccallum@hotmail.co.uk>
2017-04-01 20:39:06 -0400

    moving along a little

--- propagator.py
+++ propagator.py
@@ -4,6 +4,7 @@ from neo4j.v1 import GraphDatabase, basic_auth
 
 uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=(localconfig.DBUSERNAME, localconfig.DBPASSWORD))
+propagation_terminator = []
 
 def print_claim_by_id(id):
     with driver.session() as session:
@@ -14,3 +15,26 @@ def print_claim_by_id(id):
                 print(record["claim.body"])
 
 print_claim_by_id(25)
+
+def propagate_from_a_claim(id):
+    propagation_terminator.append(id)
+    with driver.session() as session:
+        with session.begin_transaction() as tx:
+            for record in tx.run("MATCH (claim:Claim)-->(argument:ArgGroup)<--(premis) "
+                                 "WHERE ID(claim) = {id} "
+                                 "RETURN arg", premis=premis):
+                print("hi")
+
+
+def next_claim_to_propagate():
+    print("get next claim to propagate")
+
+def next_argument_to_propagate():
+    print("get next argument to propagate")
+
+
+def calculate_claim_state_from_arguments():
+    print("calculate claim state from arguments")
+
+def calculate_argument_state_from_claims():
+    print("calculate state from argument")
